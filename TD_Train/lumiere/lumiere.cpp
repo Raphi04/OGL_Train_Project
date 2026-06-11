3e9180353e8a910eeb044f7b7da347a3994424f5
Raphi04 <raphaelcadete04@gmail.com>
2026-06-11 11:43:01 +0200

    Refactorisation

--- TD_Train/lumiere/lumiere.cpp
+++ TD_Train/lumiere/lumiere.cpp
@@ -7,10 +7,10 @@ bool lightToggle { false };
 void initLights() {
     myEngine.switchToPhongShading();
 
-	myEngine.addALight({0.f, sceneComposition.terrainHeight / 2, 15.f, 0.f}, {0.5f, 0.5f, 0.5f});
-	myEngine.addALight({sceneComposition.terrainWidth / 2, 0.f, 15.f, 0.f}, {0.5f, 0.5f, 0.5f});
-	myEngine.addALight({0.f, -sceneComposition.terrainHeight / 2, 15.f, 0.f}, {0.2f, 0.2f, 0.2f});
-	myEngine.addALight({-sceneComposition.terrainWidth / 2, 0.f, 15.f, 0.f}, {0.2f, 0.2f, 0.2f});
+	myEngine.addALight({0.f, sceneComposition.terrainHeight / 2, 15.f, 0.f}, {0.9f, 0.9f, 0.9f});
+	myEngine.addALight({sceneComposition.terrainWidth / 2, 0.f, 15.f, 0.f}, {0.9f, 0.9f, 0.9f});
+	myEngine.addALight({0.f, -sceneComposition.terrainHeight / 2, 15.f, 0.f}, {0.5f, 0.5f, 0.5f});
+	myEngine.addALight({-sceneComposition.terrainWidth / 2, 0.f, 15.f, 0.f}, {0.5f, 0.5f, 0.5f});
 
 	myEngine.setShininess(5.f);
 
--- TD_Train/rails/rails.cpp
+++ TD_Train/rails/rails.cpp
@@ -5,6 +5,17 @@ IndexedMesh* test3;
 
 GLBI_Convex_2D_Shape rail{3};
 GLBI_Convex_2D_Shape curvedRail{3};
+
+GLBI_Convex_2D_Shape curvedRail1Top{3};
+GLBI_Convex_2D_Shape curvedRail1Bot{3};
+GLBI_Convex_2D_Shape curvedRail1Left{3};
+GLBI_Convex_2D_Shape curvedRail1Right{3};
+
+GLBI_Convex_2D_Shape curvedRail2Top{3};
+GLBI_Convex_2D_Shape curvedRail2Bot{3};
+GLBI_Convex_2D_Shape curvedRail2Left{3};
+GLBI_Convex_2D_Shape curvedRail2Right{3};
+
 IndexedMesh* balast{};
 
 IndexedMesh* cube{};
@@ -184,170 +195,264 @@ void initStraightRails() {
 void initCurvedRails() {
 	float const& sr { railParams.rail_size };
 
-	std::vector<float> curvedRailShape {
-		/* Face du Bas */
-		-(sr / 2.f), 0.f, 0.f,
-		-(sr / 2.f),  sr, 0.f,
-		 (sr / 2.f), 0.f, 0.f,
-
-		 (sr / 2.f), 0.f, 0.f,
-		 (sr / 2.f),  sr, 0.f,
-		-(sr / 2.f),  sr, 0.f,
-
-		/* Face du Fond */
-		-(sr / 2.f),  sr, 0.f,
-		-(sr / 2.f),  sr,  sr,
-		 (sr / 2.f),  sr, 0.f,
-
-		 (sr / 2.f),  sr, 0.f,
-		 (sr / 2.f),  sr,  sr,
-		-(sr / 2.f),  sr,  sr,
-
-		/* Face de Gauche*/
-		-(sr / 2.f),  sr,  sr,
-		-(sr / 2.f), 0.f,  sr,
-		-(sr / 2.f),  sr, 0.f,
-
-		-(sr / 2.f),  sr, 0.f,
-		-(sr / 2.f), 0.f, 0.f,
-		-(sr / 2.f), 0.f,  sr,
-
-		/* Face Avant */
-		-(sr / 2.f), 0.f,  sr,
-		-(sr / 2.f), 0.f, 0.f,
-		 (sr / 2.f), 0.f,  sr,
-
-		 (sr / 2.f), 0.f,  sr,
-		 (sr / 2.f), 0.f, 0.f,
-		-(sr / 2.f), 0.f, 0.f,
+	/* CurvedRail1Top */
+	std::vector<float> curvedRail1TopShape {};
+	std::vector<float> curvedRail1TopNormals {};
+	std::vector<float> curvedRail1TopUvs {};
 	
-		/* Face du Dessus */
-		-(sr / 2.f),  sr,  sr,
-		 (sr / 2.f),  sr,  sr,
-		 (sr / 2.f), 0.f,  sr,
+	/* CurvedRail1Left */
+	std::vector<float> curvedRail1LeftShape {};
+	std::vector<float> curvedRail1LeftNormals {};
+	std::vector<float> curvedRail1LeftUvs {};
 
-		 (sr / 2.f), 0.f,  sr,
-		-(sr / 2.f), 0.f,  sr,
-		-(sr / 2.f),  sr,  sr,
+	/* CurvedRail1Right */
+	std::vector<float> curvedRail1RightShape {};
+	std::vector<float> curvedRail1RightNormals {};
+	std::vector<float> curvedRail1RightUvs {};
 
-		/* Face de Droit */
-		 (sr / 2.f), 0.f, 0.f,
-		 (sr / 2.f),  sr, 0.f,
-		 (sr / 2.f), 0.f,  sr,
+	/* CurvedRail1Bot */
+	std::vector<float> curvedRail1BotShape {};
+	std::vector<float> curvedRail1BotNormals {};
+	std::vector<float> curvedRail1BotUvs {};
 
-		 (sr / 2.f), 0.f,  sr,
-		 (sr / 2.f),  sr,  sr,
-		 (sr / 2.f),  sr, 0.f
-	};
+	/* CurvedRail2Top */
+	std::vector<float> curvedRail2TopShape {};
+	std::vector<float> curvedRail2TopNormals {};
+	std::vector<float> curvedRail2TopUvs {};
 
-	std::vector<float> curvedRailShapeNormals {
-		/* Face de Bas */
-		0.f, 0.f, -1.f,
-		0.f, 0.f, -1.f,
-		0.f, 0.f, -1.f,
-		0.f, 0.f, -1.f,
-		0.f, 0.f, -1.f,
-		0.f, 0.f, -1.f,
+	/* CurvedRail2Left */
+	std::vector<float> curvedRail2LeftShape {};
+	std::vector<float> curvedRail2LeftNormals {};
+	std::vector<float> curvedRail2LeftUvs {};
 
-		/* Face du Fond */
-		1.f, 0.f, 0.f,
-		1.f, 0.f, 0.f,
-		1.f, 0.f, 0.f,
-		1.f, 0.f, 0.f,
-		1.f, 0.f, 0.f,
-		1.f, 0.f, 0.f,
+	/* CurvedRail2Right */
+	std::vector<float> curvedRail2RightShape {};
+	std::vector<float> curvedRail2RightNormals {};
+	std::vector<float> curvedRail2RightUvs {};
 
-		/* Face de Gauche */
-		0.f, -1.f, 0.f,
-		0.f, -1.f, 0.f,
-		0.f, -1.f, 0.f,
-		0.f, -1.f, 0.f,
-		0.f, -1.f, 0.f,
-		0.f, -1.f, 0.f,
+	/* CurvedRail2Bot */
+	std::vector<float> curvedRail2BotShape {};
+	std::vector<float> curvedRail2BotNormals {};
+	std::vector<float> curvedRail2BotUvs {};
 
-		/* Face Avant */
-		-1.f, 0.f, 0.f,
-		-1.f, 0.f, 0.f,
-		-1.f, 0.f, 0.f,
-		-1.f, 0.f, 0.f,
-		-1.f, 0.f, 0.f,
-		-1.f, 0.f, 0.f,
+	for(int i { 0 }; i <= 50; i++) {
+		float angle = i * (M_PI /2.0f) / 50.0f;
 
-		/* Face du Haut */
-		0.f, 0.f, 1.f,
-		0.f, 0.f, 1.f,
-		0.f, 0.f, 1.f,
-		0.f, 0.f, 1.f,
-		0.f, 0.f, 1.f,
-		0.f, 0.f, 1.f,
+		float posX = cos(angle) * (railParams.posRailOne - sr / 2);
+		float posY = sin(angle) * (railParams.posRailOne - sr / 2);
 
-		/* Face de Droite */
-		0.f, 1.f, 0.f,
-		0.f, 1.f, 0.f,
-		0.f, 1.f, 0.f,
-		0.f, 1.f, 0.f,
-		0.f, 1.f, 0.f,
-		0.f, 1.f, 0.f,
-	};
+		float posXE = cos(angle) * (railParams.posRailOne + sr / 2);
+		float posYE = sin(angle) * (railParams.posRailOne + sr / 2);
 
-	std::vector<float> curvedRailShapeUvs {
-		/* Face du Bas */
-		0.f, 0.f,
-		0.f, 1.f,
-		1.f, 0.f,
+		/* CurvedRail1Top */
+		curvedRail1TopShape.push_back(posX);
+		curvedRail1TopShape.push_back(posY);
+		curvedRail1TopShape.push_back(sr);
 
-		1.f, 0.f,
-		1.f, 1.f,
-		0.f, 1.f,
+		curvedRail1TopNormals.push_back(0.f);	
+		curvedRail1TopNormals.push_back(0.f);	
+		curvedRail1TopNormals.push_back(1.f);
+		
+		curvedRail1TopUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail1TopUvs.push_back(0.4 + sin(angle) / 2 );
 
-		/* Face du Fond */
-		0.f, 0.f,
-		0.f, 1.f,
-		1.f, 0.f,
+		curvedRail1TopShape.push_back(posXE);
+		curvedRail1TopShape.push_back(posYE);
+		curvedRail1TopShape.push_back(sr);
+		
+		curvedRail1TopNormals.push_back(0.f);	
+		curvedRail1TopNormals.push_back(0.f);	
+		curvedRail1TopNormals.push_back(1.f);
 
-		1.f, 0.f,
-		1.f, 1.f,
-		0.f, 1.f,
+		curvedRail1TopUvs.push_back(0.5 + cos(angle)  / 2);	
+		curvedRail1TopUvs.push_back(0.5 + sin(angle)  / 2);
 
-		/* Face de Gauche */
-		0.f, 1.f,
-		1.f, 1.f,
-		0.f, 0.f,
+		/* CurvedRail1Left */
+		curvedRail1LeftShape.push_back(posX);
+		curvedRail1LeftShape.push_back(posY);
+		curvedRail1LeftShape.push_back(0.f);
 
-		0.f, 0.f,
-		1.f, 0.f,
-		1.f, 1.f,
+		curvedRail1LeftNormals.push_back(-cos(angle));	
+		curvedRail1LeftNormals.push_back(-sin(angle));	
+		curvedRail1LeftNormals.push_back(0.f);
+		
+		curvedRail1LeftUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail1LeftUvs.push_back(0.4 + sin(angle) / 2 );
 
-		/* Face Avant */
-		0.f, 1.f,
-		0.f, 0.f,
-		1.f, 1.f,
+		curvedRail1LeftShape.push_back(posX);
+		curvedRail1LeftShape.push_back(posY);
+		curvedRail1LeftShape.push_back(sr);
+		
+		curvedRail1LeftNormals.push_back(-cos(angle));	
+		curvedRail1LeftNormals.push_back(-sin(angle));	
+		curvedRail1LeftNormals.push_back(0.f);
 
-		1.f, 1.f,
-		1.f, 0.f,
-		0.f, 0.f,
+		curvedRail1LeftUvs.push_back(0.5 + cos(angle)  / 2);	
+		curvedRail1LeftUvs.push_back(0.5 + sin(angle)  / 2);
 
-		/* Face du Dessus */
-		0.f, 1.f,
-		1.f, 1.f,
-		1.f, 0.f,
+		/* CurvedRail1Right */
+		curvedRail1RightShape.push_back(posXE);
+		curvedRail1RightShape.push_back(posYE);
+		curvedRail1RightShape.push_back(0.f);
 
-		1.f, 0.f,
-		0.f, 0.f,
-		0.f, 1.f,
+		curvedRail1RightNormals.push_back(cos(angle));	
+		curvedRail1RightNormals.push_back(sin(angle));	
+		curvedRail1RightNormals.push_back(0.f);
+		
+		curvedRail1RightUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail1RightUvs.push_back(0.4 + sin(angle) / 2 );
 
-		/* Face de Droite */
-		0.f, 0.f,
-		0.f, 1.f,
-		1.f, 0.f,
+		curvedRail1RightShape.push_back(posXE);
+		curvedRail1RightShape.push_back(posYE);
+		curvedRail1RightShape.push_back(sr);
+		
+		curvedRail1RightNormals.push_back(cos(angle));	
+		curvedRail1RightNormals.push_back(sin(angle));	
+		curvedRail1RightNormals.push_back(0.f);
 
-		1.f, 0.f,
-		1.f, 1.f,
-		0.f, 1.f
-	};
+		curvedRail1RightUvs.push_back(0.5 + cos(angle)  / 2);	
+		curvedRail1RightUvs.push_back(0.5 + sin(angle)  / 2);
 
-	curvedRail.initShape(curvedRailShape, curvedRailShapeNormals, curvedRailShapeUvs);
-	curvedRail.changeNature(GL_TRIANGLES);
+		/* CurvedRail1Bot */
+		curvedRail1BotShape.push_back(posX);
+		curvedRail1BotShape.push_back(posY);
+		curvedRail1BotShape.push_back(0.f);
+
+		curvedRail1BotNormals.push_back(0.f);	
+		curvedRail1BotNormals.push_back(0.f);	
+		curvedRail1BotNormals.push_back(1.f);
+		
+		curvedRail1BotUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail1BotUvs.push_back(0.4 + sin(angle) / 2 );
+
+		curvedRail1BotShape.push_back(posXE);
+		curvedRail1BotShape.push_back(posYE);
+		curvedRail1BotShape.push_back(0.f);
+		
+		curvedRail1BotNormals.push_back(0.f);	
+		curvedRail1BotNormals.push_back(0.f);	
+		curvedRail1BotNormals.push_back(-1.f);
+
+		curvedRail1BotUvs.push_back(0.5 + cos(angle)  / 2);	
+		curvedRail1BotUvs.push_back(0.5 + sin(angle)  / 2);
+
+		float posX2 = cos(angle) * (railParams.posRailTwo - sr / 2);
+		float posY2 = sin(angle) * (railParams.posRailTwo - sr / 2);
+
+		float posX2E = cos(angle) * (railParams.posRailTwo + sr / 2);
+		float posY2E = sin(angle) * (railParams.posRailTwo + sr / 2);
+
+		/* CurvedRail2Top */
+		curvedRail2TopShape.push_back(posX2);
+		curvedRail2TopShape.push_back(posY2);
+		curvedRail2TopShape.push_back(sr);
+
+		curvedRail2TopNormals.push_back(0.f);	
+		curvedRail2TopNormals.push_back(0.f);	
+		curvedRail2TopNormals.push_back(1.f);
+		
+		curvedRail2TopUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail2TopUvs.push_back(0.4 + sin(angle) / 2 );
+
+		curvedRail2TopShape.push_back(posX2E);
+		curvedRail2TopShape.push_back(posY2E);
+		curvedRail2TopShape.push_back(sr);
+		
+		curvedRail2TopNormals.push_back(0.f);	
+		curvedRail2TopNormals.push_back(0.f);	
+		curvedRail2TopNormals.push_back(1.f);
+
+		curvedRail2TopUvs.push_back(0.5 + cos(angle)  / 2);	
+		curvedRail2TopUvs.push_back(0.5 + sin(angle)  / 2);
+
+		/* CurvedRail2Left */
+		curvedRail2LeftShape.push_back(posX2);
+		curvedRail2LeftShape.push_back(posY2);
+		curvedRail2LeftShape.push_back(0.f);
+
+		curvedRail2LeftNormals.push_back(-cos(angle));	
+		curvedRail2LeftNormals.push_back(-sin(angle));	
+		curvedRail2LeftNormals.push_back(0.f);
+		
+		curvedRail2LeftUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail2LeftUvs.push_back(0.4 + sin(angle) / 2 );
+
+		curvedRail2LeftShape.push_back(posX2);
+		curvedRail2LeftShape.push_back(posY2);
+		curvedRail2LeftShape.push_back(sr);
+		
+		curvedRail2LeftNormals.push_back(-cos(angle));	
+		curvedRail2LeftNormals.push_back(-sin(angle));	
+		curvedRail2LeftNormals.push_back(0.f);
+
+		curvedRail2LeftUvs.push_back(0.5 + cos(angle)  / 2);	
+		curvedRail2LeftUvs.push_back(0.5 + sin(angle)  / 2);
+
+		/* CurvedRail2Right */
+		curvedRail2RightShape.push_back(posX2E);
+		curvedRail2RightShape.push_back(posY2E);
+		curvedRail2RightShape.push_back(0.f);
+
+		curvedRail2RightNormals.push_back(cos(angle));	
+		curvedRail2RightNormals.push_back(sin(angle));	
+		curvedRail2RightNormals.push_back(0.f);
+		
+		curvedRail2RightUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail2RightUvs.push_back(0.4 + sin(angle) / 2 );
+
+		curvedRail2RightShape.push_back(posX2E);
+		curvedRail2RightShape.push_back(posY2E);
+		curvedRail2RightShape.push_back(sr);
+		
+		curvedRail2RightNormals.push_back(cos(angle));	
+		curvedRail2RightNormals.push_back(sin(angle));	
+		curvedRail2RightNormals.push_back(0.f);
+
+		curvedRail2RightUvs.push_back(0.5 + cos(angle)  / 2);	
+		curvedRail2RightUvs.push_back(0.5 + sin(angle)  / 2);
+
+		/* CurvedRail2Bot */
+		curvedRail2BotShape.push_back(posX2);
+		curvedRail2BotShape.push_back(posY2);
+		curvedRail2BotShape.push_back(0.f);
+
+		curvedRail2BotNormals.push_back(0.f);	
+		curvedRail2BotNormals.push_back(0.f);	
+		curvedRail2BotNormals.push_back(1.f);
+		
+		curvedRail2BotUvs.push_back(0.4 + cos(angle) / 2 );	
+		curvedRail2BotUvs.push_back(0.4 + sin(angle) / 2 );
+
+		curvedRail2BotShape.push_back(posX2E);
+		curvedRail2BotShape.push_back(posY2E);
+		curvedRail2BotShape.push_back(0.f);
+		
+		curvedRail2BotNormals.push_back(0.f);	
+		curvedRail2BotNormals.push_back(0.f);	
+		curvedRail2BotNormals.push_back(-1.f);
+
+		curvedRail2BotUvs.push_back(0.5 + cos(angle) / 2);	
+		curvedRail2BotUvs.push_back(0.5 + sin(angle) / 2);
+
+	}
+
+	curvedRail1Top.initShape(curvedRail1TopShape, curvedRail1TopNormals, curvedRail1TopUvs);
+	curvedRail1Top.changeNature(GL_TRIANGLE_STRIP);
+	curvedRail1Bot.initShape(curvedRail1BotShape, curvedRail1BotNormals, curvedRail1BotUvs);
+	curvedRail1Bot.changeNature(GL_TRIANGLE_STRIP);
+	curvedRail1Left.initShape(curvedRail1LeftShape, curvedRail1LeftNormals, curvedRail1LeftUvs);
+	curvedRail1Left.changeNature(GL_TRIANGLE_STRIP);
+	curvedRail1Right.initShape(curvedRail1RightShape, curvedRail1RightNormals, curvedRail1RightUvs);
+	curvedRail1Right.changeNature(GL_TRIANGLE_STRIP);
+
+	curvedRail2Top.initShape(curvedRail2TopShape, curvedRail2TopNormals, curvedRail2TopUvs);
+	curvedRail2Top.changeNature(GL_TRIANGLE_STRIP);
+	curvedRail2Left.initShape(curvedRail2LeftShape, curvedRail2LeftNormals, curvedRail2LeftUvs);
+	curvedRail2Left.changeNature(GL_TRIANGLE_STRIP);
+	curvedRail2Right.initShape(curvedRail2RightShape, curvedRail2RightNormals, curvedRail2RightUvs);
+	curvedRail2Right.changeNature(GL_TRIANGLE_STRIP);
+	curvedRail2Bot.initShape(curvedRail2BotShape, curvedRail2BotNormals, curvedRail2BotUvs);
+	curvedRail2Bot.changeNature(GL_TRIANGLE_STRIP);
 }
 
 void initBalast(){
@@ -371,8 +476,15 @@ void drawStraightRails() {
 			myEngine.mvMatrixStack.addTranslation({railParams.posRailOne, 0.f, 2 * railParams.balastRayons});
 			myEngine.updateMvMatrix();
 
-			myEngine.setFlatColor(0.6f, 0.6f, 0.6f);
-			rail.drawShape();
+			myEngine.activateTexturing(true);
+			
+				metalTexture.attachTexture();
+
+				rail.drawShape();
+
+				metalTexture.detachTexture();
+
+			myEngine.activateTexturing(false);
 
 		myEngine.mvMatrixStack.popMatrix();
 		myEngine.updateMvMatrix();
@@ -383,8 +495,15 @@ void drawStraightRails() {
 			myEngine.mvMatrixStack.addTranslation({railParams.posRailTwo, 0.f, 2 * railParams.balastRayons});
 			myEngine.updateMvMatrix();
 
-			myEngine.setFlatColor(0.6f, 0.6f, 0.6f);
-			rail.drawShape();
+			myEngine.activateTexturing(true);
+			
+				metalTexture.attachTexture();
+				
+				rail.drawShape();
+			
+				metalTexture.detachTexture();
+
+			myEngine.activateTexturing(false);
 
 		myEngine.mvMatrixStack.popMatrix();
 		myEngine.updateMvMatrix();
@@ -452,39 +571,46 @@ void drawCurvedRails() {
 		myEngine.updateMvMatrix();
 		
 		/* Dessin du premier Rail */
-		for(int i { 1 }; i <= 50; i++) {
-			float angle = i * (M_PI /2) / 50;
-			float posX = cos(angle) * railParams.posRailOne;
-			float posY = sin(angle) * railParams.posRailOne;
-
-			myEngine.mvMatrixStack.pushMatrix();
-				myEngine.mvMatrixStack.addTranslation({0.f, -railParams.rail_size / 2, 2 * railParams.balastRayons});
-				myEngine.mvMatrixStack.addTranslation({posX, posY, 0.f});
-				myEngine.updateMvMatrix();
-			
-				myEngine.setFlatColor(0.6f, 0.6f, 0.6f);
-				curvedRail.drawShape();
-
-			myEngine.mvMatrixStack.popMatrix();
+		myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addTranslation({0.f, 0.f, 2 * railParams.balastRayons});
 			myEngine.updateMvMatrix();
-		}
+
+			myEngine.activateTexturing(true);
+			
+				metalTexture.attachTexture();
+
+				curvedRail1Top.drawShape();
+				curvedRail1Left.drawShape();
+				curvedRail1Right.drawShape();
+				curvedRail1Bot.drawShape();
+
+				metalTexture.detachTexture();
+
+			myEngine.activateTexturing(false);
+
+		myEngine.mvMatrixStack.popMatrix();
+		myEngine.updateMvMatrix();
 
 		/* Dessin du deuxième Rail */
-		for(int i { 1 }; i <= 50; i++) {
-			float angle = i * (M_PI /2) / 50;
-			float posX = cos(angle) * railParams.posRailTwo;
-			float posY = sin(angle) * railParams.posRailTwo;
-
-			myEngine.mvMatrixStack.pushMatrix();
-				myEngine.mvMatrixStack.addTranslation({0.f, -railParams.rail_size / 2, 2 * railParams.balastRayons});
-				myEngine.mvMatrixStack.addTranslation({posX, posY, 0.f});
-				myEngine.updateMvMatrix();
-
-				curvedRail.drawShape();
-
-			myEngine.mvMatrixStack.popMatrix();
+		myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addTranslation({0.f, 0.f, 2 * railParams.balastRayons});
 			myEngine.updateMvMatrix();
-		}
+
+			myEngine.activateTexturing(true);
+			
+				metalTexture.attachTexture();
+
+				curvedRail2Top.drawShape();
+				curvedRail2Left.drawShape();
+				curvedRail2Right.drawShape();
+				curvedRail2Bot.drawShape();
+
+				metalTexture.detachTexture();
+
+			myEngine.activateTexturing(false);
+
+		myEngine.mvMatrixStack.popMatrix();
+		myEngine.updateMvMatrix();
 
 		/* Dessin des trois Balasts */
 		myEngine.mvMatrixStack.pushMatrix();
